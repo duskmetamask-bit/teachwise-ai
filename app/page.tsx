6e05129c2b8d9db402fece7fb0823b2242465466
Yuki <yuki@dawnlabsai.com>
2026-05-10 12:38:02 +0200

    fix: remove conflicting root page, keep (app) group routing

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-import { redirect } from 'next/navigation';
-
-export default function RootPage() {
-  redirect('/');
-}
-
-// This file exists to prevent the old app/page.tsx from being used
-// All routes are in app/(app)/ now
